f09cfbd7406e4efa6ec02c2642332ea18e9347cf
gghisellini <giovanni@pineapple-lab.com>
2022-11-29 16:09:57 -0300

    UPDATE: Show raking

--- src/pages/Rankings.js
+++ src/pages/Rankings.js
@@ -1,58 +1,50 @@
-import "./Profile.css";
-import React, { useCallback, useEffect, useState } from "react";
-import { Typography, Button, CircularProgress, TextField } from "@mui/material";
-import { supabase } from "../supabase";
+import './Profile.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Typography, Button, CircularProgress, TextField } from '@mui/material';
+import { supabase } from '../supabase';
 
 const ShowMostLikedPosts = () => {
-  const [posts, setPosts] = useState([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-  const fetchPosts = useCallback(async () => {
-    setIsLoading(true);
-      const { data, error } = await supabase
-      .rpc('get_top_5_liked_posts')
-      if (error) {
-        setError(error.message);
-      } else {
-        setPosts(data)
-        console.log(data);
-      }
-    setIsLoading(false);
-  });
+    const fetchPosts = useCallback(async () => {
+        setIsLoading(true);
+        await supabase
+            .rpc('get_top_5_liked_posts')
+            .then(({ data, error }) => {
+                if (error) {
+                    setError(error.message);
+                }
+                setPosts(data);
+            })
+            .finally(() => setIsLoading(false));
+    }, []);
 
-  useEffect(() => {
-    fetchPosts();
-  }, [fetchPosts]);
+    useEffect(() => {
+        fetchPosts();
+    }, [fetchPosts]);
 
-  return (
-    <>
-      {error && <Typography color="error">{error}</Typography>}
-      {isLoading ? (
-        <CircularProgress />
-      ) : (
-        <>
-          {posts.length === 0 ? (
-            <div className="empty-feed">
-              <Typography>Aún no hay publicaciones...</Typography>
-            </div>
-          ) : (
-            posts.map((post) => (
-              <div key={post.post_id}>
-                <div className="post-container">
-                  <div className="row-container">
-                    <div className="row-elements-flex">
-                      <Typography variant="h6">{post.full_name}</Typography>
-
-                      <Typography variant="body2">{`Subido el ${
-                        `${post.created_at}`.split("T")[0] +
-                        " " +
-                        "a las " +
-                        `${post.created_at}`.split("T")[1].split(".")[0]
-                      }`}</Typography>
-                    </div>
-                  </div>
-                  <img
+    return (
+        <div className="container">
+            <Typography variant="h4" component="h1" gutterBottom>
+                Most Liked Posts
+            </Typography>
+            {isLoading && <CircularProgress />}
+            {error && (
+                <Typography variant="h6" component="h2" gutterBottom>
+                    {error}
+                </Typography>
+            )}
+            {posts.map((post) => (
+                <div key={post.id} className="post">
+                    <Typography variant="h6" component="h2" gutterBottom>
+                        {post.title}
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        {post.description}
+                    </Typography>
+                    <img
                     src={post.image}
                     alt="Imágen del post"
                     style={{
@@ -64,18 +56,16 @@ const ShowMostLikedPosts = () => {
                       paddingTop: 5,
                     }}
                   />
+                    <Typography variant="body2" gutterBottom>
+                        {post.likes}
+                    </Typography>
                 </div>
-              </div>
-            ))
-          )}
-        </>
-      )}
-    </>
-  );
+            ))}
+        </div>
+    );
 };
 
 
 export default function Rankings() {
   return <ShowMostLikedPosts />;
 }
-
